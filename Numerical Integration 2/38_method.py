a0e590ab4c0df0a7ed7d0f16adea899859f6252c
ilyparkr <109953354+ilyparkr@users.noreply.github.com>
2026-03-05 22:47:24 +0700

    Update 38_method.py

--- Numerical Integration 2/38_method.py
+++ Numerical Integration 2/38_method.py
@@ -2,7 +2,7 @@
 
 import numpy as np
 def method1():
-    x = np.arange(13)
+    x = np.linspace(np.pi/2, 3*np.pi/2, 13)
     y = np.cos(x)**3
     n = len(x)-1
     h = (x[-1] - x[0]) / n
@@ -23,5 +23,16 @@ def method2():
     integral = 3*h / 8 * (y[0] + 3*np.sum(y[1:12:3] + y[2:n:3]) + 2*np.sum(y[3:n-2:3]) + y[-1])
     return integral
 
+def method3():
+    x = np.linspace(np.pi/2, 3*np.pi/2, 1000) # สมมุติโจทย์ให้ n = 12 ต้องบวก 1 เพิ่มด้วยเพราะ n คือจำนวนแท่ง
+    y = np.cos(x)**3
+    n = len(x)-1
+    h = (x[-1] - x[0]) / n
+    integral = 0
+    for i in range(0, n, 3):
+        integral += (3*h / 8) * (y[i] + 3*y[i+1] + 3*y[i+2] + y[i+3])
+    return integral
+
 print("method1 : ", method1())
 print("method2 : ", method2())
+print("method3 : ", method3())
