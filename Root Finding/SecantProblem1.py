e91662d1e87ab5b068d91552ead9f60756787c67
RedH0tCh1||1Pepper$ <65008138+Redandh0t@users.noreply.github.com>
2026-03-09 21:43:41 +0700

    Add files via upload

---
+++ Root Finding/SecantProblem1.py
@@ -0,0 +1,19 @@
+import numpy as np
+
+Cminus1 = 10
+C0 = 20
+MaxIteration = 3
+Tole = 0.000000001
+def f(c):
+    return ((668.061/c) * (1-np.exp(-0.14684*c))) - 40
+
+for i in range(MaxIteration):
+    Cplus1 = C0 -((f(C0)*(C0-Cminus1))/(f(C0) - f(Cminus1)))
+    if np.abs(f(C0)) > Tole:
+        Cminus1 = C0
+        C0 = Cplus1
+        print("Iteration = ",i)
+        print("f(c) = ",f(C0))
+        print("Result = ",Cplus1)
+    else:
+        break
